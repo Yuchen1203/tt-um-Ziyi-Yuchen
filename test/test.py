5d0e02202a3c05d28a377fff64a976052bd1c536
Yuchen1203 <161077993+Yuchen1203@users.noreply.github.com>
2024-04-18 08:28:15 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -12,22 +12,25 @@ from cocotb.binary import BinaryValue
 async def test_pwm_with_reset_and_timing(dut):
     dut._log.info("Start")
 
-    clock = Clock(dut.clk, 1, units="ns")
+    clock = Clock(dut.clk, 10, units="ns")
     cocotb.start_soon(clock.start())
-
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 20)
-    dut.rst_n.value = 1
-    await ClockCycles(dut.clk, 20)
+    
     dut.ui_in[1].value = 0
     dut.ui_in[0].value = 0
-    await ClockCycles(dut.clk, 5)
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 1)
+
+    dut.ui_in[0].value = 0
     dut.ui_in[0].value = 1
-    await ClockCycles(dut.clk, 5)
-    
+    await ClockCycles(dut.clk, 1000)
+
+
+    '''
     for i in range(20):
         print(dut.uo_out[0].value)
-        await ClockCycles(dut.clk, 10)
-            
+        await ClockCycles(dut.clk, 1)
+    '''
             
             
