f8d74aa3b1762bf4669d71a98278cf551d8a45f2
Yuchen1203 <161077993+Yuchen1203@users.noreply.github.com>
2024-04-18 09:32:28 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -20,20 +20,18 @@ async def test_pwm_with_reset_and_timing(dut):
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 10)
 
     dut.ui_in[0].value = 1
 
     await ClockCycles(dut.clk, 10)
-    
-    dut.ui_in[0].value = 0
-    dut.ui_in[0].value = 1
-    
+
+    dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
-    
-    dut.ui_in[0].value = 0
-    dut.ui_in[0].value = 1
-    
+    dut.rst_n.value = 1
+
+    await ClockCycles(dut.clk, 10)
+
     
 
 
