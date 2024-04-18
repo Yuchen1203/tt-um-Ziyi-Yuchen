52eb9387cf0674d865d78c71e2656de075f20ca2
Yuchen1203 <161077993+Yuchen1203@users.noreply.github.com>
2024-04-18 09:01:18 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -22,7 +22,6 @@ async def test_pwm_with_reset_and_timing(dut):
     dut.rst_n.value = 1
     await ClockCycles(dut.clk, 1)
 
-    dut.ui_in[0].value = 0
     dut.ui_in[0].value = 1
     
     await ClockCycles(dut.clk, 1)
@@ -46,32 +45,6 @@ async def test_pwm_with_reset_and_timing(dut):
     await ClockCycles(dut.clk, 1)
     print(dut.uo_out[0].value)
     
-    await ClockCycles(dut.clk, 1)
-    dut.ui_in[0].value = 0
-    dut.ui_in[0].value = 1
-    
-    await ClockCycles(dut.clk, 1)
-    print(dut.uo_out[0].value)
-    await ClockCycles(dut.clk, 1)
-    print(dut.uo_out[0].value)
-    await ClockCycles(dut.clk, 1)
-    print(dut.uo_out[0].value)
-    await ClockCycles(dut.clk, 1)
-    print(dut.uo_out[0].value)
-    await ClockCycles(dut.clk, 1)
-    print(dut.uo_out[0].value)
-    await ClockCycles(dut.clk, 1)
-    print(dut.uo_out[0].value)
-    await ClockCycles(dut.clk, 1)
-    print(dut.uo_out[0].value)
-    await ClockCycles(dut.clk, 1)
-    print(dut.uo_out[0].value)
-    await ClockCycles(dut.clk, 1)
-    print(dut.uo_out[0].value)
-    await ClockCycles(dut.clk, 1)
-    print(dut.uo_out[0].value)
-
-    await ClockCycles(dut.clk, 1)
     dut.ui_in[0].value = 0
     dut.ui_in[0].value = 1
     
@@ -96,7 +69,30 @@ async def test_pwm_with_reset_and_timing(dut):
     await ClockCycles(dut.clk, 1)
     print(dut.uo_out[0].value)
 
+    dut.ui_in[0].value = 0
+    dut.ui_in[0].value = 1
+    
     await ClockCycles(dut.clk, 1)
+    print(dut.uo_out[0].value)
+    await ClockCycles(dut.clk, 1)
+    print(dut.uo_out[0].value)
+    await ClockCycles(dut.clk, 1)
+    print(dut.uo_out[0].value)
+    await ClockCycles(dut.clk, 1)
+    print(dut.uo_out[0].value)
+    await ClockCycles(dut.clk, 1)
+    print(dut.uo_out[0].value)
+    await ClockCycles(dut.clk, 1)
+    print(dut.uo_out[0].value)
+    await ClockCycles(dut.clk, 1)
+    print(dut.uo_out[0].value)
+    await ClockCycles(dut.clk, 1)
+    print(dut.uo_out[0].value)
+    await ClockCycles(dut.clk, 1)
+    print(dut.uo_out[0].value)
+    await ClockCycles(dut.clk, 1)
+    print(dut.uo_out[0].value)
+
     dut.ui_in[0].value = 0
     dut.ui_in[0].value = 1
     
