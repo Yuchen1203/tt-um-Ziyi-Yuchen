42a432ca356c0daae834a0e4db34e5bdeb4ab977
Yuchen1203 <161077993+Yuchen1203@users.noreply.github.com>
2024-04-18 09:06:27 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -42,11 +42,10 @@ async def test_pwm_with_reset_and_timing(dut):
     print(dut.uo_out[0].value)
     await ClockCycles(dut.clk, 1)
     print(dut.uo_out[0].value)
+    dut.ui_in[0].value = 0
     await ClockCycles(dut.clk, 1)
     print(dut.uo_out[0].value)
     
-    dut.ui_in[0].value = 0
-    await ClockCycles(dut.clk, 1)
     dut.ui_in[0].value = 1
     
     await ClockCycles(dut.clk, 1)
@@ -67,11 +66,10 @@ async def test_pwm_with_reset_and_timing(dut):
     print(dut.uo_out[0].value)
     await ClockCycles(dut.clk, 1)
     print(dut.uo_out[0].value)
+    dut.ui_in[0].value = 0
     await ClockCycles(dut.clk, 1)
     print(dut.uo_out[0].value)
 
-    dut.ui_in[0].value = 0
-    await ClockCycles(dut.clk, 1)
     dut.ui_in[0].value = 1
     
     await ClockCycles(dut.clk, 1)
@@ -92,11 +90,10 @@ async def test_pwm_with_reset_and_timing(dut):
     print(dut.uo_out[0].value)
     await ClockCycles(dut.clk, 1)
     print(dut.uo_out[0].value)
+    dut.ui_in[0].value = 0
     await ClockCycles(dut.clk, 1)
     print(dut.uo_out[0].value)
 
-    dut.ui_in[0].value = 0
-    await ClockCycles(dut.clk, 1)
     dut.ui_in[0].value = 1
     
     await ClockCycles(dut.clk, 1)
